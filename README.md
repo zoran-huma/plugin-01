2d705efc25af7d607f2b75340f155fac36e6c6df
Zoran <zoran.rasic@huma.com>
2022-06-30 13:57:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+To generate a build file
+
+npx vue-cli-service build --target lib --formats umd-min --no-clean --dest ./build --name "plugin-01.[chunkhash]" src/App.vue
+
+Then copy the *umd.min.js and *css file to the server/build folder of the server
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(0);
+
+    return {
+      count,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="counter-wrapper">
+    <h1>Plugin 02</h1>
+    <h2>{{ count }}</h2>
+    <div class="buttons-wrapper">
+      <button @click="count--">Decrease</button>
+      <button @click="count++">Increase</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.counter-wrapper {
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background: #2E7D32;
+  color: white;
+  padding: 2rem;
+}
+</style>
